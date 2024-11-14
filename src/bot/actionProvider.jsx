d659cc07d520fba036a6e9f2a3ce8bce6b4e55c2
vitoria0325 <sophiacarolinelopes02@gmail.com>
2024-11-13 21:55:42 -0300

    inicio do chat bot .

--- src/bot/actionProvider.jsx
+++ src/bot/actionProvider.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const initialAction = () => {
+    const message = createChatBotMessage('Insira o seu nome para começa!');
+    updateState(messagem)
+  }
+
+  const updateState = (message) => {
+    setState((prev) => ({
+      ...prev,
+      messages:[...prev.messages, message],
+    }));
+  }
+
+
   return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child, {
-          actions: {},
+          actions: { 
+            initialAction
+          },
         });
       })}
     </div>
